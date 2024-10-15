44089332604394967e396fd4c18b98d801dfd877
hsc996 <han.scaife@gmail.com>
2024-10-15 20:58:54 +1100

    feat: file structure changed for testing purposes

--- src/index.js
+++ src/index.js
@@ -1,32 +1,7 @@
-// Import express
-const express = require("express");
+const {app} = require("./server.js");
 
-// Make an instance of the Express system
-const app = express();
-
-const PORT = 3000;
-
-// When we receive a request on localhost:3000/ (GET)
-app.get("/", (request, response) => {
-    // response.send("<h1>Hello world</h1>"); // This is an example of server-side rendering (we will not be doing this)
-    response.json({
-        message: "Hello world"
-    })
-});
-
-app.post("/", (request, response) => {
-    response.json({
-        message: "POST request received"
-    });
-});
-
-
-const {router} = require("./controllers/pokemonController.js");
-// localhost:3000/pokemon/
-app.use("/pokemon", router);
-
+const PORT = process.env.PORT ?? 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server listening on localhost:${PORT}`);
+	console.log(`Server listening on localhost:${PORT}`);
 });
-
